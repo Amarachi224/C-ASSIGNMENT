9bb54cb01c6b27830a94915324f1344d6062e21f
information2020 <afrienduned4real@gmail.com>
2015-05-22 16:09:21 +0100

    C++ ASSIGNMENT
    PROGRAM CODES

---
+++ QUESTION3.cpp
@@ -0,0 +1,32 @@
+/*
+*	Question3.cpp
+*
+*	Name:				[OLEWUNNE, AMARACHI BENEDICTA ]
+*	Department:			[MATHEMATICS]
+*	Matric./Reg. No:	[47569255JA]
+*	
+*	a program that prints a box, an oval, an arrow and a diamond.
+*	
+*/
+
+#include <iostream>
+
+int main ()
+{
+	std::cout << "Welcome\n";
+	std::cout << "This program prints a box, an oval, an arrow and a diamond.\n\n";
+	
+	std::cout << "*********\t\t" << "   ***   \t\t" << "  *  \t\t" << "     *     \n";
+	std::cout << "*       *\t\t" << " *     *\t\t"  << " *** \t\t" << "    * *    \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "*****\t\t" << "   *   *   \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << "  *     *  \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << " *       * \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << "*         *\n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << " *       * \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << "  *     *  \n"
+		 	  << "*       *\t\t" << "*       *\t\t" << "  *  \t\t" << "   *   *   \n"
+		 	  << "*       *\t\t" << " *     *\t\t"  << "  *  \t\t" << "    * *    \n"
+		 	  << "*********\t\t" << "   ***  \t\t"  << "  *  \t\t" << "     *     \n";
+		 
+		 	  return 0;
+}
